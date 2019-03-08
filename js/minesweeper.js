9e3962ffe99245446c386deb8d68fa2255638889
Irina <irina.schubert@students.ffhs.ch>
2019-03-08 14:26:54 +0100

    replace mockup; add some basic html and css

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -6,4 +6,23 @@
 
 'use strict';
 
+function startGame() {
+    myGameArea.start();
+}
 
+let myGameArea = {
+    
+}
+
+// setup fields
+let field = document.getElementsByClassName("field");
+let fields = [...field];
+// loop to add event listeners to each field
+for (let i = 0; i < fields.length; i++){
+   fields[i].addEventListener("click", displayField);
+};
+
+let displayField = function (){
+   this.classList.toggle("open");
+   this.classList.toggle("flagged");
+}
