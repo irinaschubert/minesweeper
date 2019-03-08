23981b549b50e713f383974af47f90a0ab24bbb7
Irina <irina.schubert@students.ffhs.ch>
2019-03-08 16:44:16 +0100

    add some basic styles, elements and structure

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -6,14 +6,6 @@
 
 'use strict';
 
-function startGame() {
-    myGameArea.start();
-}
-
-let myGameArea = {
-    
-}
-
 // setup fields
 let field = document.getElementsByClassName("field");
 let fields = [...field];
@@ -25,4 +17,67 @@ for (let i = 0; i < fields.length; i++){
 let displayField = function (){
    this.classList.toggle("open");
    this.classList.toggle("flagged");
+}
+
+const panel = document.querySelector(".panel");
+
+function startGame(level) {
+    let level = level;
+    for (var i= 0; i < level; i++){
+      [].forEach.call(fields, function(item){
+         panel.appendChild(item);
+      });
+   }
+}
+
+function clickField() {
+    // check if field is a mine or if player wins, continue otherwise
+    if(){
+        // looseGame()
+        } else if (){
+        // winGame()
+        }else{
+        // continue
+    }
+};
+       
+function looseGame(){
+    // open popup
+}
+
+function winGame(){
+    // open popup
+}
+
+function countFlags(){    
+    oneUp++;    
+    counter.innerHTML = oneUp;
+}
+
+function restartGame(){
+    //reset timer
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 mins 0 secs";
+    clearInterval(interval);
+    startGame();
+}
+
+//game timer
+//TODO: start timer on first click
+let second = 0, minute = 0;
+let timer = document.querySelector(".timer");
+let interval;
+function startTimer(){
+    interval = setInterval(function(){
+        timer.innerHTML = minute+"mins "+second+"secs";
+        second++;
+        if(second == 60){
+            minute++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour++;
+            minute = 0;
+        }
+    },1000);
 }
